151684fc31b4dd6e6c3986f2ccc385b655ced7d2
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2019-11-11 13:30:57 +0100

    Tagging prog Exercises

--- programming/4-lists/1-average/average.py
+++ programming/4-lists/1-average/average.py
@@ -2,8 +2,13 @@ from typing import List, Optional
 
 
 def average(my_list: List[int]) -> Optional[float]:
-    length = len(my_list)
+    length: int = len(my_list)
+
     if length == 0:
         return None
-    else:
-        return sum(my_list) / length
+
+    element_sum: int = 0
+    for element in my_list:
+        element_sum += element
+
+    return element_sum / length
